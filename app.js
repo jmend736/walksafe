4cae7b9c426bece60fcbddc2cda6083b5ac9e057
jmend <jmend@mit.edu>
2016-09-17 14:44:26 -0400

    Created a basic express app

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+
+app.use(express.static('static'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/static/templates/index.html'));
+});
+
+app.listen(1337, function () {
+  console.log('Server is running!');
+});
